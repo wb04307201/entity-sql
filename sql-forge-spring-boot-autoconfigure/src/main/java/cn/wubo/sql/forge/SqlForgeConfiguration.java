768b9ec9ef45ae70d64721b379d17b1e1678075a
吴博 <wubo_aaa@163.com>
2025-12-03 17:33:16 +0800

    feat(sql-forge): 配置Kingbase数据库连接并增强路由功能
    - 添加了Kingbase数据库的连接配置信息
    - 更新依赖项从sql-forge-core到sql-forge-spring-boot-autoconfigure
    - 扩展了SQL Forge的路由配置，支持获取数据库元数据
    - 新增获取数据库目录、模式及表信息的GET接口
    - 支持通过查询参数过滤模式和表列表

--- sql-forge-spring-boot-autoconfigure/src/main/java/cn/wubo/sql/forge/SqlForgeConfiguration.java
+++ sql-forge-spring-boot-autoconfigure/src/main/java/cn/wubo/sql/forge/SqlForgeConfiguration.java
@@ -22,8 +22,22 @@ public class SqlForgeConfiguration {
     }
 
     @Bean("sqlForgeRouter")
-    public RouterFunction<ServerResponse> sqlForgeRouter() {
-        RouterFunctions.Builder builder =RouterFunctions.route();
+    public RouterFunction<ServerResponse> sqlForgeRouter(MetaData metaData) {
+        RouterFunctions.Builder builder = RouterFunctions.route();
+        builder.GET("/sql/forge/database", request -> ServerResponse.ok().body(metaData.getDatabase()));
+        builder.GET("/sql/forge/database/catalogs", request -> ServerResponse.ok().body(metaData.getCatalogs()));
+        builder.GET("/sql/forge/schemas", request -> {
+            String catalog = request.param("catalog").orElse(null);
+            String schemaPattern = request.param("schemaPattern").orElse(null);
+            return ServerResponse.ok().body(metaData.getSchemas(catalog, schemaPattern));
+        });
+        builder.GET("/sql/forge/tables", request -> {
+            String catalog = request.param("catalog").orElse(null);
+            String schemaPattern = request.param("schemaPattern").orElse(null);
+            String tableNamePattern = request.param("tableNamePattern").orElse(null);
+            String[] types = request.param("types").map(typesStr -> typesStr.split(",")).orElse(null);
+            return ServerResponse.ok().body(metaData.getTables(catalog, schemaPattern, tableNamePattern, types));
+        });
         return builder.build();
     }
 
