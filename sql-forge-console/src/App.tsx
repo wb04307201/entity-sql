17633f260632c2876afde7b1fc3e938d0e0999ed
吴博 <wubo_aaa@163.com>
2025-12-08 20:10:13 +0800

    feat(App): 实现可编辑标签页功能
    - 引入 Tabs 组件并配置可编辑卡片类型
    - 添加标签页增删逻辑
    - 设置默认激活及不可关闭标签页
    - 实现标签页切换与内容展示
    - 支持通过点击新增按钮创建新标签页
    - 支持通过关闭按钮移除现有标签页

--- sql-forge-console/src/App.tsx
+++ sql-forge-console/src/App.tsx
@@ -1,13 +1,83 @@
-import {Layout} from 'antd';
+import {Layout, Tabs, type TabsProps} from 'antd';
+import {useRef, useState} from "react";
 
 const {Content, Sider} = Layout;
 
+type TargetKey = React.MouseEvent | React.KeyboardEvent | string;
+
+const initialItems = [
+    { label: 'Tab 1', children: 'Content of Tab 1', key: '1' },
+    { label: 'Tab 2', children: 'Content of Tab 2', key: '2' },
+    {
+        label: 'Tab 3',
+        children: 'Content of Tab 3',
+        key: '3',
+        closable: false,
+    },
+];
+
 function App() {
+
+    const [activeKey, setActiveKey] = useState(initialItems[0].key);
+    const [items, setItems] = useState(initialItems);
+    const newTabIndex = useRef(0);
+
+    const onChange = (newActiveKey: string) => {
+        setActiveKey(newActiveKey);
+    };
+
+    const add = () => {
+        const newActiveKey = `newTab${newTabIndex.current++}`;
+        const newPanes = [...items];
+        newPanes.push({ label: 'New Tab', children: 'Content of new Tab', key: newActiveKey });
+        setItems(newPanes);
+        setActiveKey(newActiveKey);
+    };
+
+    const remove = (targetKey: TargetKey) => {
+        let newActiveKey = activeKey;
+        let lastIndex = -1;
+        items.forEach((item, i) => {
+            if (item.key === targetKey) {
+                lastIndex = i - 1;
+            }
+        });
+        const newPanes = items.filter((item) => item.key !== targetKey);
+        if (newPanes.length && newActiveKey === targetKey) {
+            if (lastIndex >= 0) {
+                newActiveKey = newPanes[lastIndex].key;
+            } else {
+                newActiveKey = newPanes[0].key;
+            }
+        }
+        setItems(newPanes);
+        setActiveKey(newActiveKey);
+    };
+
+    const onEdit = (
+        targetKey: React.MouseEvent | React.KeyboardEvent | string,
+        action: 'add' | 'remove',
+    ) => {
+        if (action === 'add') {
+            add();
+        } else {
+            remove(targetKey);
+        }
+    };
+
 
     return (
         <Layout style={{height: '100%'}}>
             <Sider theme={'light'}>left sidebar</Sider>
-            <Content>main content</Content>
+            <Content>
+                <Tabs
+                    type="editable-card"
+                    onChange={onChange}
+                    activeKey={activeKey}
+                    onEdit={onEdit}
+                    items={items}
+                />
+            </Content>
         </Layout>
     )
 }
